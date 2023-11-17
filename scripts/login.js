17502490605b7ff9562661ebefa92b87638dee4b
nesta.web <nesta.web.dev@gmail.com>
2023-11-17 14:33:53 +0100

    test

---
+++ scripts/login.js
@@ -0,0 +1,60 @@
+// frontend/scripts/login.js
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3001/api'; // Update this to the correct server port
+
+export async function authenticateUser() {
+  try {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await axios.post(`${apiUrl}/authenticate`, {
+      username,
+      password,
+    });
+
+    const token = response.data.token;
+    console.log('Token JWT obtained:', token);
+
+    // Store the token securely, e.g., in a cookie or local storage
+    window.localStorage.setItem('jwtToken', token);
+
+    // Redirect to the admin page
+    window.location.href = '/admin';
+  } catch (error) {
+    console.error('Authentication failed', error);
+    alert('Authentication failed. Please check your credentials.');
+  }
+}
+
+export async function fetchData() {
+  const token = getJwtToken();
+
+  try {
+    const response = await axios.get(`${apiUrl}/admin`, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Ensure 'Bearer' is prepended to the token
+      },
+    });
+
+    console.log('Admin data:', response.data);
+  } catch (error) {
+    console.error('Failed to fetch admin data', error);
+  }
+}
+
+// Function to retrieve the JWT token from local storage
+function getJwtToken() {
+  return window.localStorage.getItem('jwtToken');
+}
+
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  authenticateUser();
+});
+
+// Ensure that the authenticateUser function is available globally
+window.authenticateUser = authenticateUser;
+
+// Example: Make authenticated request after successful login
+fetchData();
