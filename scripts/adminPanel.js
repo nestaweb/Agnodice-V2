cce11d7d2120cc42773b9de5ebcd200989f93e01
nesta.web <nesta.web.dev@gmail.com>
2023-11-21 15:58:50 +0100

    save new texts

--- scripts/adminPanel.js
+++ scripts/adminPanel.js
@@ -27,6 +27,47 @@ export async function authenticateUser() {
   }
 }
 
+export async function fetchSpecificText(key) {
+	try {
+	  const response = await axios.get(`${apiUrl}/data/${key}`);
+	  const textData = response.data;
+	  return textData[key];
+	} catch (error) {
+	  console.error(`Failed to fetch text for key '${key}' from /api/data`, error);
+	  throw error; // Rethrow the error to propagate it up
+	}
+}
+
+async function fillData() {
+	const elements = document.querySelectorAll('.admin__text');
+  
+	for (const element of elements) {
+		
+		try {
+		  	const key = element.dataset.key;
+		  	if (key) {
+			  	const text = await fetchSpecificText(key);
+		  
+		 		// Assuming you want to set the text content of the element
+
+				const titleElement = element.querySelector(".admin__text__title");
+		
+				const title = text.title;
+				titleElement.innerText = title || '';
+				console.log(`Updated textarea for key: ${key}`);
+				
+			  	const textareaElement = element.querySelector("textarea");
+		  
+				const content = text.content;
+				textareaElement.innerText = content || '';
+				console.log(`Updated textarea for key: ${key}`);
+			}
+		} catch (error) {
+		  console.error(`Error fetching or updating text`, error);
+		}
+	}		
+  }
+
 // Example function for making an authenticated request
 export async function fetchData() {
   const token = getJwtToken();
@@ -44,6 +85,7 @@ export async function fetchData() {
     });
 
     console.log('Admin data:', response.data);
+    fillData();
   } catch (error) {
     console.error('Failed to fetch admin data', error);
   }
@@ -54,5 +96,61 @@ function getJwtToken() {
   return window.localStorage.getItem('jwtToken');
 }
 
+async function saveChange() {
+	document.querySelectorAll('.admin__text').forEach((element) => {
+		const textarea = element.querySelector("textarea");
+		const button = element.querySelector("button");
+		
+		textarea.addEventListener("input", async () => {
+			try {
+				const text = await fetchSpecificText(element.dataset.key);
+				const textareaContent = textarea.value;
+				const originalTextContent = text.content ? text.content.trim() : '';
+		
+				if (textareaContent !== originalTextContent) {
+					button.classList.add("admin__button--active");
+					button.disabled = false;
+				} else {
+					button.disabled = true;
+					button.classList.remove("admin__button--active");
+				}
+			} catch (error) {
+				console.error('Error fetching text:', error);
+			}
+		});
+			
+		button.addEventListener("click", async () => {
+			try {
+			const key = element.dataset.key;
+			const content = textarea.value;
+		
+			// Send updated content to the backend
+			await updateTextOnBackend(key, content);
+		
+			// Reset button state
+			button.classList.remove("admin__button--active");
+			button.disabled = true;
+		
+			console.log(`Updated content for key: ${key}`);
+			} catch (error) {
+			console.error('Error updating content:', error);
+			}
+		});
+	});
+}
+	
+async function updateTextOnBackend(key, content) {
+	try {
+		// Send a request to the backend to update the content
+		await axios.put(`${apiUrl}/data/${key}`, { content });
+	} catch (error) {
+		console.error('Error updating content on the backend:', error);
+		throw error;
+	}
+}
+
+
+
 // Call fetchData when the script is loaded (you may want to trigger this based on user actions)
 fetchData();
+saveChange();
